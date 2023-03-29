5c200de09c044bd807d156c258cc2d65daa00b5d
SovathChean <sovathchean09@gmail.com>
2023-03-29 07:59:54 +0700

    genearte khqr

--- src/main/java/com/sovathc/mongodemocrud/common/utils/ImageUtils.java
+++ src/main/java/com/sovathc/mongodemocrud/common/utils/ImageUtils.java
@@ -21,7 +21,9 @@ public class ImageUtils {
             Integer innerWidth,
             Integer innerHeight,
             Integer x,
-            Integer y) {
+            Integer y,
+            String extension,
+            Integer coefficient) {
         // Frame
         byte[] famedata =
                 DatatypeConverter.parseBase64Binary(frameImage.substring(frameImage.indexOf(",") + 1));
@@ -30,25 +32,30 @@ public class ImageUtils {
         byte[] innerdata =
                 DatatypeConverter.parseBase64Binary(innerImage.substring(innerImage.indexOf(",") + 1));
         BufferedImage inner = ImageIO.read(new ByteArrayInputStream(innerdata));
-        Font font = new Font("Inter",  Font.PLAIN, 12);
+        Font font = new Font("Inter",  Font.PLAIN, 12*coefficient);
+        int type = BufferedImage.TYPE_INT_RGB;
+        switch (extension) {
+            case "png": type = BufferedImage.TYPE_INT_ARGB;
+                break;
+            default: break;
+        }
         // New Image
         BufferedImage modified =
-                new BufferedImage(frameWidth, frameHeight, BufferedImage.TYPE_INT_ARGB);
+                new BufferedImage(frameWidth*2, frameHeight*2, type);
         Graphics2D graphics = (Graphics2D) modified.getGraphics();
-        graphics.drawImage(frame, 0, 0, frameWidth, frameHeight, null);
-        graphics.drawImage(inner, x, y, innerWidth, innerHeight, null);
+        graphics.drawImage(frame, 0, 0, frameWidth*coefficient, frameHeight*coefficient, null);
+        graphics.drawImage(inner, x*coefficient, y*coefficient, innerWidth*coefficient, innerHeight*coefficient, null);
         graphics.setFont(font);
         graphics.setColor(Color.BLACK);
-        graphics.drawString("User Name", 30, 66);
-        graphics.setFont(new Font("Inter", Font.BOLD, 21));
-        graphics.drawString("$ X,XXX", 30, 96);
+        graphics.drawString("SO PSP KHR Supplier CO,. LTD SO PSP KHR Supplier CO,. LTD ", 30*2, 66*2);
+        graphics.setFont(new Font("Inter", Font.BOLD, 21*coefficient));
+        graphics.drawString("$ 1,000,000,000,000", 30*coefficient, 96*coefficient);
         graphics.dispose();
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ImageIO.write(modified, "PNG", out);
+        ImageIO.write(modified, extension, out);
         byte[] bytes = out.toByteArray();
-        String base64bytes = Base64Utils.encodeToString(bytes);
-        return "data:image/png;base64," + base64bytes;
+        return Base64Utils.encodeToString(bytes);
     }
     @SneakyThrows
     public static final String decorate(
@@ -76,8 +83,8 @@ public class ImageUtils {
         BufferedImage modified =
                 new BufferedImage(frameWidth, frameHeight, type);
         Graphics2D graphics = (Graphics2D) modified.getGraphics();
-        graphics.drawImage(inner, 0, frameHeight - frameWidth, frameWidth, frameHeight, Color.WHITE, null);
-        graphics.drawImage(frame, 0, 0, frameWidth, headerHeight, Color.WHITE, null);
+        graphics.drawImage(inner, 0, frameHeight - frameWidth, frameWidth, frameWidth, Color.WHITE, null);
+        graphics.drawImage(frame, 0, 0, frameWidth, frameHeight, Color.WHITE, null);
         graphics.dispose();
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
